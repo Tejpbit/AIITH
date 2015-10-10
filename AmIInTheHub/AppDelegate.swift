e941a7a7b7fda2d8d4768045062b09c65b2105e7
Andr√© Samuelsson <tejp@andres-mbp.chalmers.it>
2015-10-10 22:14:30 +0200

    Initial Commit

---
+++ AmIInTheHub/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AmIInTheHub
+//
+//  Created by André Samuelsson on 10/10/15.
+//  Copyright © 2015 André Samuelsson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
